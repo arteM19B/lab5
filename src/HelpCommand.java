b3f75f5f004390ebf708f6175a70be49e367e8e0
arteM19B <bahetkin.artem@gmail.com>
2026-03-03 12:38:57 +0300

    Add "help command"

---
+++ src/HelpCommand.java
@@ -0,0 +1,20 @@
+import java.util.Map;
+
+public class HelpCommand implements Command {
+    private Map<String, Command> commands;
+    public HelpCommand(Map<String, Command> commands) {
+        this.commands = commands;
+    }
+
+    @Override
+    public void execute() {
+        for (Map.Entry<String, Command> entry : commands.entrySet()) {
+            System.out.println(entry.getKey() + " -- " + entry.getValue());
+        }
+    }
+
+//    @Override
+//    public String toString() {
+//
+//    }
+}
