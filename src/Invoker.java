3301c4c069cc3f78b6253d9b8afb3eb074eeefa7
arteM19B <bahetkin.artem@gmail.com>
2026-03-03 12:35:19 +0300

    Add Invoker

---
+++ src/Invoker.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Invoker {
+    private Map<String, Command> commandsMap = new HashMap<>();
+    public void registerCommand(String name, Command command) {
+            commandsMap.put(name, command);
+    }
+
+    public Command getCommand(String name) {
+        return commandsMap.get(name);
+    }
+
+    public boolean HasCommand(String name) {
+        return commandsMap.containsKey(name);
+    }
+}
