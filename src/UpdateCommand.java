ce3278a182d6aa149227a07914ba0491ddc8f495
arteM19B <bahetkin.artem@gmail.com>
2026-03-03 12:36:43 +0300

    Add "update" command

---
+++ src/UpdateCommand.java
@@ -0,0 +1,12 @@
+public class UpdateCommand implements Command {
+    CollectionManager collectionManager;
+
+    public UpdateCommand(CollectionManager collectionManager) {
+        this.collectionManager = collectionManager;
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
