f12c86fa64c0aab4e77d595f482dd1b644f2b2df
arteM19B <bahetkin.artem@gmail.com>
2026-03-03 12:38:18 +0300

    Add "info" command

---
+++ src/InfoCommand.java
@@ -0,0 +1,33 @@
+public class InfoCommand implements Command {
+    private CollectionManager collectionManager;
+
+    public InfoCommand(CollectionManager collectionManager) {
+        this.collectionManager = collectionManager;
+    }
+
+    //
+
+    @Override
+    public void execute() {
+        System.out.println("Тип: " + collectionManager.getType());
+        System.out.println("Дата инициализации: " + collectionManager.
+                getInitializationTime());
+        System.out.println("Размер: " + collectionManager.size());
+
+        if (collectionManager.getFileName() != null) {
+            System.out.println("Имя коллекции: " + collectionManager.getFileName());
+        }
+
+        if (collectionManager.size() > 0) {
+            System.out.println(collectionManager.getCollection().
+                    getFirst().getClass().getSimpleName());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "InfoCommand";
+    }
+
+
+}
